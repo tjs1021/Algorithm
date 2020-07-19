16f1a2b0a8ce7d3c2bfad823892e4f7c51c43e3d
tjs1021 <68340245+tjs1021@users.noreply.github.com>
2020-07-19 21:55:57 +0900

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -8,11 +8,10 @@ size : 스택에 들어있는 정수의 개수를 출력한다.
 empty : 스택이 비어있으면 1, 아니면 0을 출력한다.
 top : 스택의 가장 위에 있는 정수를 출력한다.만약 스택에 들어있는 정수가 없는 경우에는 - 1을 출력한다.
 */
-#define MAX_STACK_SIZE 100
 int top = -1;
-int stack[MAX_STACK_SIZE];
+int stack[10001];
 
-int empty() {  //스택이 비어있다면 1 리턴
+int empty() {  //스택이 비어있다면 1 리턴한다
 	if (top <= -1)
 		return 1;
 	else
@@ -20,32 +19,32 @@ int empty() {  //스택이 비어있다면 1 리턴
 
 }
 
-int full() { //스택이 꽉 찼다면 1 리턴
-	if (top == (MAX_STACK_SIZE - 1))
+int full() { //스택이 꽉 찼다면 1을 리턴한다
+	if (top == (10001 - 1))
 		return 1;
 	else
 		return 0;
 
 }
 
-void push(int a) {
+void push(int a) { //스택이 꽉 찼는지 체크하고 push한다
 	if (full())
-		cout << "스택이 꽉 찼습니다";
+		cout << "스택이 꽉 찼습니다"<<endl;
 	else
-	    stack[++top] = a;
+		stack[++top] = a;
 
 }
 
-void pop() {
+void pop() { //스택이 비어있는지 체크하고 pop한다
 	if (empty())
-		cout << "스택이 비었습니다";
+		cout << "스택이 비었습니다"<<endl;
 	else
 		stack[top--];
 }
 
-int peek() {
+int peek() { //스택의 가장 위의 정수를 리턴한다
 	if (empty()) {
-		cout << "스택이 비어있습니다";
+		cout << "스택이 비어있습니다" << endl;
 		return 0;
 	}
 	else
@@ -54,33 +53,35 @@ int peek() {
 
 
 int main() {
+	int n; //명령의 수
+	int x; //push x
 	int check_empty;
 	int check_top;
-	int x;
-	while (true) {
+
+	cout << "명령의 수를 입력하세요>>";
+	cin >> n;
+	for (int i = 0; i < n; i++) { //입력받은 명령의 수만큼 for문이 실행
 		char input[100];
 		cout << "명령을 입력하세요>>";
 		cin >> input;
-		if (strcmp(input, "push") == 0) {
+		if (strcmp(input, "push") == 0) { //stack에서 push를 한다
 			cin >> x;
 			push(x);
 		}
-		else if (strcmp(input, "pop") == 0)
+		else if (strcmp(input, "pop") == 0) //stack에서 pop을 한다
 			pop();
-		else if (strcmp(input, "size") == 0)
+		else if (strcmp(input, "size") == 0) //stack의 size를 출력한다
 			cout << top + 1 << endl;
-		else if (strcmp(input, "empty") == 0) {
+		else if (strcmp(input, "empty") == 0) { //stack이 비어있는지 체크한다(비어있다면 1 출력)
 			check_empty = empty();
 			cout << check_empty << endl;
 		}
-		else if (strcmp(input, "top") == 0) {
+		else if (strcmp(input, "top") == 0) { //stack의 가장 위에 값을 출력한다
 			check_top = peek();
-			cout << check_top<<endl;
+			cout << check_top << endl;
 		}
-		else
-			cout << "제대로입력하세요\n";
-			
 
 	}
-	
+	cout << "입력할 수 있는 명령의 수가 끝났습니다" << endl;
+
 }
